6c020466e2e0ee7c7999000565d1ed1cf0586c7e
Ramon <pcnay2003@gmail.com>
2019-12-10 00:18:27 -0700

    Iniciando la barra de paginador en la pantalla de Lista Usuarios

--- sistema/lista_usuario.php
+++ sistema/lista_usuario.php
@@ -29,9 +29,38 @@
 
       <?php
         //$query = mysqli_query($conection,"SELECT u.idusuario,u.nombre,u.correo,u.usuario,r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE u.estatus=1");
-        $consulta = new Conexion();
-        $consulta->query = "SELECT u.idusuario,u.nombre,u.email,u.usuario,u.cumpleanos,u.id_rol,r.rol FROM t_Usuarios u INNER JOIN t_Rol r ON u.id_rol = r.id_rol WHERE u.estatus = '1'";
-        //$consulta->get_query();       
+        //$consulta->get_query();  
+        
+        // Seccion del paginador 
+        //$sql_registe = mysqli_query ($conection,"SELECT COUNT(*) AS total_registro FROM usuario WHERE estatus=1");
+        //$result_register = mysqli_fetch_array($sql_registe);
+        //$total_registro = $result_register['total_registro'];
+        $conectar = new Conexion();
+        $conectar->query = "SELECT COUNT(*) AS total_registro FROM t_Usuarios WHERE estatus='1'";
+        //print_r ($conectar->query);
+        //exit;
+
+        $datos = $conectar->get_query();
+
+        $total_registro = $datos[0]['total_registro'];
+
+        $por_pagina = 3;
+        
+        // Este valor es que se pasara por la URL, cuando se oprime un número del paginador.
+        if (empty ($_GET['pagina']))
+        {
+          $pagina = 1;
+        }
+        else
+        {
+          $pagina = $_GET['pagina'];
+        }
+
+        $desde = ($pagina-1)*$por_pagina;
+        $total_paginas = ceil($total_registro/$por_pagina);
+
+        //$query = mysqli_query($conection,"SELECT u.idusuario,u.nombre,u.correo,u.usuario,r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE u.estatus = 1 ORDER BY u.nombre ASC LIMIT $desde,$por_pagina");        
+
   /*
         if (!empty($consulta->rows))        
         {
@@ -45,8 +74,14 @@
           }  
 */
 
-          $datos2 = $consulta->get_query();
+          $consulta = new Conexion();
+          $consulta->query = "SELECT u.idusuario,u.nombre,u.email,u.usuario,u.cumpleanos,r.rol FROM t_Usuarios u INNER JOIN t_Rol r ON u.id_rol = r.id_rol WHERE u.estatus = '1' ORDER BY u.nombre ASC LIMIT $desde,$por_pagina";
+          //print_r ($consulta->query);
+          //exit;
 
+          $datos2 = $consulta->get_query();
+          //print_r (count($datos2));
+          //exit;
         
           for ($n=0;$n<count($datos2);$n++)
           {
@@ -60,19 +95,13 @@
               <td><?php echo $datos2[$n]['rol']; ?></td>
               <td>
                 <!-- Se pasa el "Id" del usuario al archivo "editar_usuario"-->
-                <a class="link_edit" href="editar_usuario.php?id=<?php echo $datos2[$n]['idusuario']; ?>">Editar</a>
-                
-                <!-- NO permite borrar al Usuario Administrador principal. -->
-                <?php if ($datos2[$n]['idusuario'] != 1) { ?>
-                    |              
-                    <a class="link_delete" href="eliminar_usuario.php?id=<?php echo $datos2[$n]['idusuario']; ?>">Eliminar</a>
-                <?php } ?>
+                <a class="link_edit" href="editar_usuario.php?id=<?php echo $datos2[$n]['idusuario']; ?>">Editar</a>             
+                |              
+                <a class="link_delete" href="eliminar_usuario.php?id=<?php echo $datos2[$n]['idusuario']; ?>">Eliminar</a>
               </td>
             </tr>
       <?php      
-        } // if (!empty($conectar->rows))
-
-         // } // for ($n=0;$n<count($datos2);$n++)
+          } // for ($n=0;$n<count($datos2);$n++)         
       ?>
 
     </table>
@@ -82,11 +111,13 @@
       <ul>
         <li><a href="#">|<</a></li>
         <li><a href="#"><<</a></li>
-        <li class="pageSelected" >1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-        <li><a href="#">4</a></li>
-        <li><a href="#">5</a></li>
+        <?php 
+          for ($i=1;$i<=$total_paginas;$i++)
+          {
+            // pagina = Esta variable es la que se pasa en la URL.
+            echo '<li><a href="?pagina='.$i.'">'.$i.'</a></li>';
+          }
+        ?>
         <li><a href="#">>></a></li>
         <li><a href="#">>|</a></li>
       </ul>
